f0a0293bf22be0e7d27b178e201c4c5ebbc3a5d5
EmilianoJGit98 <ejg250398@gmail.com>
2025-05-25 14:11:31 -0300

    se elimino completamente la libreria de font-awesome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import '@fortawesome/fontawesome-free/css/all.min.css';
 
 @Component({
   selector: 'app-root',
